23f57c0efd31603ebb20b5eedadf9cc2a2f0741d
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-18 05:26:43 +0100

    Update exercise3.cpp

--- Programacion 1/Clase 1/exercise3.cpp
+++ Programacion 1/Clase 1/exercise3.cpp
@@ -8,9 +8,11 @@ using namespace std;
 int main(){
     int numero_entero;
     cout<<"Ingrese un numero entero de 3 digitos: "; cin>>numero_entero;
+
     while(numero_entero<100||numero_entero>999){
         cout<<"Numero invalido, ingrese nuevamente: "; cin>>numero_entero;
     }
+
     int numero_alrevez=0;
     while(numero_entero>0){
         numero_alrevez=numero_alrevez*10+numero_entero%10;
