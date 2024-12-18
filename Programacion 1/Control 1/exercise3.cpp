e49332820877659476ba9f65a20b70961d3eb85e
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-18 05:15:04 +0100

    Create exercise3.cpp

---
+++ Programacion 1/Control 1/exercise3.cpp
@@ -0,0 +1,12 @@
+/*
+1. Un corredor demora x segundos en dar la vuelta a la pista atlética, se sabe que cada 3 vueltas 
+su rendimiento decrece demorándose un 20% más del tiempo. En base a lo anterior se pide 
+construir un programa que lea el número de vueltas que dará y el tiempo que demora en la 
+primera vuelta, el programa debe imprimir el tiempo que demorará en dar la n vueltas. 
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    
+}
