ec05ba9b99a2e4695fa9d944e58c0245809e84d5
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-19 05:20:07 +0100

    Create exercise5.cpp

---
+++ Programacion 1/Control 1/exercise5.cpp
@@ -0,0 +1,7 @@
+/*
+10. Construya una aplicación que lea una secuencia de números ascendentes, la secuencia 
+termina cuando se ingresa un número menor al anterior. Al finalizar debe imprimir: 
+a. Primer número 
+b. Último Número 
+c. Promedio 
+*/
