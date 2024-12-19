8af23f93789ef809e00947bdb49769f2bf3045f9
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-19 05:20:09 +0100

    Create exercise4.cpp

---
+++ Programacion 1/Control 1/exercise4.cpp
@@ -0,0 +1,13 @@
+/*
+7. Construya una aplicación que lea el numerador y denominador de una fracción, su aplicación 
+debe simplificar si corresponde la fracción. Ejemplo: 
+Ingrese numerador: 6 
+Ingrese denominador: 8 
+Al simplificar 
+Numerador: 3 
+Denominador: 4 
+*/
+
+
+#include <iostream>
+using namespace std;
