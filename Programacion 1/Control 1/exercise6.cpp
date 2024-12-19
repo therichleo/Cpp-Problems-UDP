79db18bda5c147c03aed63c063bcaf6a4432d6d5
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-19 05:20:04 +0100

    Create exercise6.cpp

---
+++ Programacion 1/Control 1/exercise6.cpp
@@ -0,0 +1,8 @@
+/*
+14. Construya una aplicación que evalúe la función -x2 + 100x entre el intervalo entero [0, 100], e 
+indique cuántos valores son: 
+a. Menores a 100 
+b. >100 y < 200 
+c. >= 200 y < 300 
+d. > 400
+*/
