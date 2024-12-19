dd14bd7d83b26f34130be40eeb0654caf832e3f1
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-19 05:20:02 +0100

    Create exercise7.cpp

---
+++ Programacion 1/Control 1/exercise7.cpp
@@ -0,0 +1,11 @@
+/*
+19. En teoría de números, la conjetura de Collatz (Lothar Collazt, 1937) de un número (N) , entero 
+positivo se define de la siguiente manera: 
+Si el número es par se divide por 2, para obtener (N/2). 
+Si el número es impar se multiplica por 3 y se le suma 1, para obtener (3*N + 1) 
+El procedimiento se repite por los números resultantes hasta llegar a 1. 
+La conjetura indica que independiente el número con que se inicie, siempre se llegará a 1 y la 
+secuencia final 4, 2, 1 siempre aparecerá. También es conocida como conjetura 3N + 1. 
+Programe una aplicación que lea un número entero positivo e imprima la secuencia de Collatz 
+respectiva hasta llegar a 1
+*/
