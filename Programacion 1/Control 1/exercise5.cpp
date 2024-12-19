ab19cc34a00d7540aa6b35efd26dcaf7cac2aba9
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-19 05:40:19 +0100

    Update exercise5.cpp

--- Programacion 1/Control 1/exercise5.cpp
+++ Programacion 1/Control 1/exercise5.cpp
@@ -5,3 +5,21 @@ a. Primer número
 b. Último Número 
 c. Promedio 
 */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int numero, siguiente, total, ultimo, iteraciones=1;
+    cout<<"Ingrese un numero: "; cin>>numero;
+    total = numero;
+    cout<<"Ingrese otro numero: "; cin>>siguiente;
+    iteraciones++;
+    ultimo = siguiente;
+    total = total + siguiente;
+    if(siguiente<numero){
+        cout<<"primer numero: "<<numero<<endl;
+        cout<<"ultimo numero: "<<ultimo<<endl;
+        cout<<"promedio: "<<total/iteraciones;
+    }
+    while
+}
