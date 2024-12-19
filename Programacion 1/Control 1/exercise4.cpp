5930a7c5ed5fc9ca6bc55a031e4eeb73ef40d1e9
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-19 05:40:21 +0100

    Update exercise4.cpp

--- Programacion 1/Control 1/exercise4.cpp
+++ Programacion 1/Control 1/exercise4.cpp
@@ -11,3 +11,33 @@ Denominador: 4
 
 #include <iostream>
 using namespace std;
+
+int main() {
+    int numerador, denominador;
+    
+    cout << "Ingrese numerador: "; 
+    cin >> numerador;
+    cout << "Ingrese denominador: "; 
+    cin >> denominador;
+
+    while (true) {
+        bool dividido = false;
+        if (numerador % 2 == 0 && denominador % 2 == 0) {
+            numerador /= 2;
+            denominador /= 2;
+            dividido = true;
+        }
+        if (numerador % 3 == 0 && denominador % 3 == 0) {
+            numerador /= 3;
+            denominador /= 3;
+            dividido = true;
+        }
+        if (!dividido) {
+            break;
+        }
+    }
+    cout << "Numerador simplificado: " << numerador << endl;
+    cout << "Denominador simplificado: " << denominador << endl;
+    return 0;
+}
+
