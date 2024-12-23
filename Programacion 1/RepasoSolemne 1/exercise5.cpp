22c58b801fbd2ece099230e5f52a3b9ae4d44200
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-23 20:00:36 +0100

    Create exercise5.cpp

---
+++ Programacion 1/RepasoSolemne 1/exercise5.cpp
@@ -0,0 +1,8 @@
+/*
+Capicúa
+ Capicúa hace referencia a cualquier número de dos o más dígitos que
+ se lee igual de izquierda a derecha y de derecha a izquierda. Un
+ ejemplo de esto es el número 2992. Cree un programa que sea capaz
+ de leer un número "N" ingresado por teclado, y defina si es capicúa o
+ no.
+*/
