2b2d0c5a87367ebe6a9e3faf85d80fc406017d58
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-23 20:00:39 +0100

    Create exercise3.cpp

---
+++ Programacion 1/RepasoSolemne 1/exercise3.cpp
@@ -0,0 +1,13 @@
+/*
+Conjetura de Collatz
+ La conjetura de collatz consiste en lo siguiente: Considere un número
+ "N" cualquiera, para este ejercicio los dos últimos digitos de su rut
+ (sin contar verificador). Si este número "N" es impar, deberá
+ multiplicarlo por 3 y sumarle 1. Si el número "N" es par, deberá
+ dividirlo por dos. Cree un programa que deberá hacer continuamente
+ estas operaciones hasta "N" tenga un valor igual a 1, donde entonces
+ imprimirá:
+ Número de veces que se multiplicó el número "N".
+ Número de veces que se dividió el número "N".
+
+*/
