bcb76a4252922be5c17d3eaa0189041eff6dcd93
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-23 19:58:18 +0100

    Update exercise.cpp

---
+++ Programacion 1/RepasoSolemne 1/exercise.cpp
@@ -0,0 +1,17 @@
+/*
+Rut mágico
+ Su número de la suerte se calcula de la siguiente manera:
+ auxiliar = (Año de nacimiento + mes de nacimiento + día de
+ nacimiento) % 100. Si el valor auxiliar < 9 entonces: Número de la
+ suerte es auxiliar + 10. De lo contrario: Número de la suerte es el valor
+ auxiliar. Se pide, cree una aplicación que lea una fecha de nacimiento
+ en formato AAAAMMDD, un rut sin dígito verificador e indique si el
+ rut contiene o no su número de la suerte, en tal caso se dirá que el rut
+ es mágico, de lo contrario será normal.
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+
+}
