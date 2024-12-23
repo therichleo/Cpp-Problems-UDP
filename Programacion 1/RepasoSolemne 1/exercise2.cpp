63e54efe962d5b0b62098742f3ccbd4b583b0c52
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-23 20:00:41 +0100

    Create exercise2.cpp

---
+++ Programacion 1/RepasoSolemne 1/exercise2.cpp
@@ -0,0 +1,14 @@
+/*
+Números Espejo
+ Un reciente estudio reveló que los llamados números espejos poseen propiedades
+ matemáticas relevantes para el dominio de la computación cuántica . Un número
+ espejo, es un número compuesto de n cifras, donde n siempre es un número impar.
+ Además, sus cifras siguen un patrón de repetición donde el último dígito es igual al
+ primero, el penúltimo es igual al segundo, el antepenúltimo es igual al tercero y así
+ sucesivamente, siguiendo un patrón similar a que existiese un espejo en su cifra
+ central. Por ejemplo el numero 1237321 es un numero espejo.
+ Considere un numero de 9 dígitos ingresado por el usuario y valide si este cumple
+ con la propiedad espejo. Si cumple, debe imprimir por pantalla: ”Es espejo”, en
+ caso contrario deberá imprimir: ”No es espejo”.
+
+*/
