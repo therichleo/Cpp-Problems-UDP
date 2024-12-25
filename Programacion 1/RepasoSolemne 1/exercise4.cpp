96e0700d1bd3421024c592ef8d6c5bf561467b08
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-12-25 03:54:19 +0100

    Update exercise4.cpp

---
+++ Programacion 1/RepasoSolemne 1/exercise4.cpp
@@ -0,0 +1,16 @@
+/*
+Tío Lucho
+ El Tío Lucho tiene dos especialidades, vender paltas y tomates. Este
+ ha tenido un mes difícil y decide ir a la feria y no volver hasta que
+ haya vendido una cantidad de $500MIL o más. Considere que el Tío
+ Lucho vende cada kg de Palta a $5MIL y cada kg de tomate a $2MIL.
+ Cree un programa que permita al Tío Lucho registrar cada una de sus
+ ventas en Kg y le diga cuántos Kg de paltas y tomates le faltan vender
+ para llegar a su meta.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int palta,tomate;
+    float venta;
+}
